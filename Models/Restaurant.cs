6e1eb7c7df714b9b40134d6f2683870346bee058
ezrafrost7 <71141893+ezrafrost7@users.noreply.github.com>
2021-02-08 15:17:58 -0700

    Add project files.

---
+++ Models/Restaurant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _413_4.Models
+{
+    public class Restaurant
+    {
+        [Required]
+        public int Rank { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string FavDish { get; set; } = "It's all tasty!";
+        [Required]
+        public string Address { get; set; }
+        [Phone]
+        public string PhoneNum { get; set; }
+        public string WebLink { get; set; } = "Coming Soon.";
+
+    }
+}
